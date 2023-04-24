8a975849432b2d811dfde00330bb351af85801ba
Leonid Vegera <leonid.vegera@gmail.com>
2023-04-24 23:54:41 +0300

    Added CORS

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import {authRouter} from "./routes/authRouter.js";
+import cors from 'cors';
 
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 
 app.use(express.json(), authRouter)
 app.listen(PORT, () => {
