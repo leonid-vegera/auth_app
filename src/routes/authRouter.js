1caee1138061402a764441e524b1e316c10b95c0
Leonid Vegera <leonid.vegera@gmail.com>
2023-05-01 19:54:23 +0300

    Save refreshToken to DB

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -7,3 +7,4 @@ export const authRouter = express.Router();
 authRouter.post('/registration', catchError(authController.register));
 authRouter.get('/activation/:activationToken', catchError(authController.activate));
 authRouter.post('/login', catchError(authController.login));
+authRouter.get('/refresh', catchError(authController.refresh));
